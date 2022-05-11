eb8207ef94ef72444265b3470361300374bfa873
08kahnn@gmail.com <08kahnn@gmail.com>
2022-05-11 21:24:34 +0500

    first commit

---
+++ src/Views/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Common/Header";
+import Form from "../../components/Auth/Form";
+import "../../assets/css/formComponent.css";
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
+};
+export default Login;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/style.css";
+import Login from "./Views/Login/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+ReactDOM.render(
+  <>
+    <Login />
+  </>,
+  document.getElementById("root")
+);
